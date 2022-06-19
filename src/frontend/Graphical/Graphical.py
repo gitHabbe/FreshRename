405af294d8b8f77b9a314207eae03470507bec80
Habbe <91habbe@gmail.com>
2022-06-19 20:14:03 +0200

    🚚 Rename: pythonic names

--- src/frontend/Graphical/Graphical.py
+++ src/frontend/Graphical/Graphical.py
@@ -7,16 +7,16 @@ class Graphical:
         self.root = Tk()
         self.root.geometry("500x400")
         self.root.title("FreshRename")
-        self.__centerFrame()
+        self.__center_frame()
         self.__addExitEvent()
 
-    def __centerFrame(self):
-        windowWidth = self.root.winfo_reqwidth()
-        windowHeight = self.root.winfo_reqheight()
-        print("Width:", windowWidth, "Height:", windowHeight)
-        positionRight = int(self.root.winfo_screenwidth() / 2 - windowWidth / 2) - 200
-        positionDown = int(self.root.winfo_screenheight() / 2 - windowHeight / 2) - 200
-        self.root.geometry(f"+{positionRight}+{positionDown}")
+    def __center_frame(self):
+        window_width = self.root.winfo_reqwidth()
+        window_height = self.root.winfo_reqheight()
+        print("Width:", window_width, "Height:", window_height)
+        position_right = int(self.root.winfo_screenwidth() / 2 - window_width / 2) - 200
+        position_down = int(self.root.winfo_screenheight() / 2 - window_height / 2) - 200
+        self.root.geometry(f"+{position_right}+{position_down}")
 
     def __addExitEvent(self):
         self.root.bind("<Escape>", lambda x: self.root.destroy())
