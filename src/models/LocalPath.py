c10ded4fd2f6ea72a2a426f4984b481026b77bdc
Habbe <91habbe@gmail.com>
2022-10-17 14:29:26 +0200

    ♻️ Ref: dataclass implementation and less private attributes

--- src/models/LocalPath.py
+++ src/models/LocalPath.py
@@ -1,23 +1,19 @@
 import json
+from dataclasses import dataclass
+from typing import ClassVar
 
 
+@dataclass
 class LocalPath:
-    file_path = "./private/cache.json"
+    file_path: ClassVar[str] = "./private/cache.json"
 
-    def __init__(self):
-        self.__jsonData = self.read_path()
+    def get_path(self):
+        with open(self.file_path, "r") as file:
+            return json.load(file)["last_path"]
 
-    def read_path(self):
-        with open(self.file_path, "r") as f:
-            __jsonData = json.load(f)
-        return __jsonData
-
-    def path(self) -> str:
-        cached_path = self.__jsonData["last_path"]
-        return cached_path
-
-    def save_path(self, path: str) -> str:
-        self.__jsonData["last_path"] = path
-        with open(self.file_path, "w") as f:
-            json.dump(self.__jsonData, f)
-        return path
+    def save_path(self, path: str):
+        current_path = self.get_path()
+        current_path["last_path"] = path
+        with open(self.file_path, "w") as file:
+            json.dump(current_path, file)
+            return current_path["last_path"]
