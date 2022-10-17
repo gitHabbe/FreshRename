40bcb56f6a1f10a73c923d2ee80db6b92f9ee409
Habbe <91habbe@gmail.com>
2022-10-17 14:24:39 +0200

    ✨ Add: dataclasses support

--- src/models/LocalFile.py
+++ src/models/LocalFile.py
@@ -1,21 +1,22 @@
 from os import DirEntry
-import re
+from re import Match
+from dataclasses import dataclass
 
 
+@dataclass
 class LocalFile:
-    def __init__(self, entry: DirEntry, match: re.match) -> None:
-        self.entry = entry
-        self.match = match
-        self.uid = self.__uid()
+    entry: DirEntry
+    match: Match
+
+    @property
+    def uid(self) -> list:
+        season = self.match.group(1).zfill(2)
+        episode = self.match.group(2).zfill(2)
+        file_type = self.entry.name.split(".")[-1]
+        file_type = file_type if file_type == "srt" else ""
+        return [ season, episode, file_type ]
 
     def path(self) -> str:
         path = self.entry.path
         path = path.split("\\")[:-1]
         return "/".join(path)
-
-    def __uid(self) -> list:
-        season = self.match.group(1).zfill(2)
-        episode = self.match.group(2).zfill(2)
-        file_type = self.entry.name.split(".")[-1]
-        file_type = file_type if file_type == "srt" else ""
-        return [season, episode, file_type]
