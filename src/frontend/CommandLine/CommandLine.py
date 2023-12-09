95c00b407033d352ef704957fbc7fd5f02793702
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-12-09 12:49:44 +0100

    ♻ Ref: move and rename methods in CommandLine

--- src/frontend/CommandLine/CommandLine.py
+++ src/frontend/CommandLine/CommandLine.py
@@ -1,6 +1,8 @@
 import questionary
 
+from models.DirTraverse import DirTraverse
 from models.LocalPath import LocalPath
+from models.Rename import Rename
 from models.RequestShow import RequestShow
 from models.namePatterns.NamePattern import NameStrategy
 from models.namePatterns.PatternSelector import patterns
@@ -11,36 +13,57 @@ class CommandLine:
     __localPath = LocalPath()
     __requestShow = RequestShow()
 
-    def choose_show_name(self) -> str:
+    def run(self):
+        show_name = self.__choose_show_name()
+        show_response = self.__confirm_show(show_name)
+        is_old_path = self.__confirm_path()
+        if not is_old_path:
+            self.__new_path()
+        episodes = self.__request_show_data(show_response)
+        episodes_path = self.__episodes_path()
+        name_strategy = self.__choose_pattern()
+        dir_traverse = DirTraverse(episodes_path)
+        dir_traverse.build_cache()
+        rename = Rename(dir_traverse, episodes, name_strategy)
+        rename.fill_file_list()
+        self.__list_changes(rename.fileList)
+        is_confirmed = self.__confirm_rename(rename.fileList)
+        if is_confirmed:
+            rename.rename_files()
+        else:
+            print("No changes made")
+            exit()
+
+    def __choose_show_name(self) -> str:
         return self.__cli.text("TV-show search:").ask()
 
-    def confirm_show(self, show_name) -> list:
-        show_response = self.__requestShow.name(show_name)[0]
-        show_name: str = show_response['show']['name']
-        user_confirm: bool = self.__cli.confirm(f"Is '{show_name}' correct TV-show?").ask()
-        if not user_confirm:
-            new_show = self.choose_show_name()
-            self.confirm_show(new_show)
-        return show_response
-
-    def confirm_path(self) -> bool:
+    def __confirm_path(self) -> bool:
         cached_path: str = self.__localPath.get_path()
         print(f"Last path: {cached_path}")
         same_path = "Use same path?"
         return self.__cli.confirm(same_path).ask()
 
-    def new_path(self) -> str:
+    def __confirm_show(self, show_name) -> list:
+        show_response = self.__requestShow.name(show_name)[0]
+        show_name: str = show_response['show']['name']
+        user_confirm: bool = self.__cli.confirm(f"Is '{show_name}' correct TV-show?").ask()
+        if not user_confirm:
+            new_show = self.__choose_show_name()
+            self.__confirm_show(new_show)
+        return show_response
+
+    def __new_path(self) -> str:
         new_path: str = self.__cli.text("Input new path:").ask()
         self.__localPath.save_path(new_path)
         return new_path
 
-    def request_show_data(self, show_response) -> list:
+    def __request_show_data(self, show_response) -> list:
         return self.__requestShow.episodes(show_response)
 
-    def episodes_path(self):
+    def __episodes_path(self):
         return self.__localPath.get_path()["last_path"]
 
-    def choose_pattern(self) -> NameStrategy:
+    def __choose_pattern(self) -> NameStrategy:
         pattern_options = list(patterns.keys())
         chosen_pattern: str = self.__cli.select(
             message="Select pattern",
@@ -50,16 +73,16 @@ class CommandLine:
         name_pattern_class = patterns.get(chosen_pattern)
         return name_pattern_class()
 
-    def confirm_rename(self, file_list: list) -> bool:
+    def __confirm_rename(self, file_list: list) -> bool:
         if len(file_list) == 0:
             print("\033[91m" + "No changes found" + "\033[0;0m")
             return False
         return self.__cli.confirm("Rename files?").ask()
 
     @staticmethod
-    def list_changes(file_list: list) -> None:
+    def __list_changes(file_list: list) -> None:
         for fileItem in file_list:
             old = f"Old: {fileItem['oldName']}"
-            new = f"New: {fileItem['newName']}"
             print(old)
+            new = f"New: {fileItem['newName']}"
             print(new)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from frontend.CommandLine.CommandLine import CommandLine
 # from frontend.Graphical.Graphical import Graphical
-from models.DirTraverse import DirTraverse
-from models.Rename import Rename
 # from src.frontend.Graphical.Body import Body
 # from src.frontend.Graphical.FormWrapper import FormWrapper
 
@@ -11,32 +9,14 @@ class FreshRename:
     @staticmethod
     def run_command_line():
         command_line = CommandLine()
-        show_name = command_line.choose_show_name()
-        show_response = command_line.confirm_show(show_name)
-        is_old_path = command_line.confirm_path()
-        if not is_old_path:
-            command_line.new_path()
-        episodes = command_line.request_show_data(show_response)
-        episodes_path = command_line.episodes_path()
-        name_strategy = command_line.choose_pattern()
-        dir_traverse = DirTraverse(episodes_path)
-        dir_traverse.build_cache()
-        rename = Rename(dir_traverse, episodes, name_strategy)
-        rename.fill_file_list()
-        command_line.list_changes(rename.fileList)
-        is_confirmed = command_line.confirm_rename(rename.fileList)
-        if is_confirmed:
-            rename.rename_files()
-        else:
-            print("No changes made")
-            exit()
+        command_line.run()
 
-    @staticmethod
-    def run_graphical():
-        graphical = Graphical()
-        body = Body(graphical.root)
-        form_wrapper = FormWrapper(body.frame)
-        graphical.loop()
+    # @staticmethod
+    # def run_graphical():
+        # graphical = Graphical()
+        # body = Body(graphical.root)
+        # form_wrapper = FormWrapper(body.frame)
+        # graphical.loop()
 
 
 if __name__ == '__main__':
