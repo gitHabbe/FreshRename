b41a85c121f1c244ec63fd0d673564928691f896
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-12-29 11:27:26 +0100

    🧹 Cln: stop referencing WIP GUI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
 from frontend.CommandLine.CommandLine import CommandLine
-# from frontend.Graphical.Graphical import Graphical
-# from src.frontend.Graphical.Body import Body
-# from src.frontend.Graphical.FormWrapper import FormWrapper
 
 
 class FreshRename:
@@ -10,13 +7,6 @@ class FreshRename:
     def run_command_line():
         command_line = CommandLine()
         command_line.run()
-
-    # @staticmethod
-    # def run_graphical():
-        # graphical = Graphical()
-        # body = Body(graphical.root)
-        # form_wrapper = FormWrapper(body.frame)
-        # graphical.loop()
 
 
 if __name__ == '__main__':
