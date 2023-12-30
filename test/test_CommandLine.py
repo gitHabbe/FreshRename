6dd9b7ea32065eb1a506945c4da8088734e1a329
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-12-30 18:37:54 +0100

    ✅ Test: split single test into 2 separate ones

--- test/test_CommandLine.py
+++ test/test_CommandLine.py
@@ -9,16 +9,22 @@ from models.RequestShow import RequestShow
 
 
 class TestCommandLine(unittest.TestCase):
-
     def setUp(self):
         self.mock_request_show_adapter = RequestShowAdapter(RequestShow())
-        self.mock_questionary_adapter = QuestionaryAdapter(questionary, "Invincible")
-        self.command_line = CommandLine(self.mock_request_show_adapter, self.mock_questionary_adapter)
 
-    def test_init(self):
-        self.command_line.run()
-        command_line_data = self.__format_dict_list(self.command_line.test_data)
-        verify(command_line_data)
+    def test_tv_show_entourage(self):
+        mock_questionary_adapter = QuestionaryAdapter(questionary, "Entourage", "C:\Entourage")
+        command_line = CommandLine(self.mock_request_show_adapter, mock_questionary_adapter)
+        command_line.run()
+        episode_list_data_as_string = self.__format_dict_list(command_line.test_data)
+        verify(episode_list_data_as_string)
+
+    def test_tv_show_invincible(self):
+        mock_questionary_adapter = QuestionaryAdapter(questionary, "Invincible", "C:\Invincible.2021.S01")
+        command_line = CommandLine(self.mock_request_show_adapter, mock_questionary_adapter)
+        command_line.run()
+        episode_list_data_as_string = self.__format_dict_list(command_line.test_data)
+        verify(episode_list_data_as_string)
 
     @staticmethod
     def __format_dict_list(episode_list):
@@ -44,12 +50,18 @@ class RequestShowAdapter:
         self.request_show = request_show
 
     @staticmethod
-    def name(tv_show_term: str):
+    def name(tv_show_term: str) -> list:
         tv_show_list = [
             {
                 "show": {
                     "id": 2013997,
                     "name": "Invincible",
+                }
+            },
+            {
+                "show": {
+                    "id": 559,
+                    "name": "Entourage",
                 }
             }
         ]
@@ -59,16 +71,21 @@ class RequestShowAdapter:
         return tv_show_list
 
     @staticmethod
-    def episodes(tv_show):
-        with open("../test/RequestShowData.json", "r") as mockFile:
-            return json.load(mockFile)
+    def episodes(tv_show) -> list:
+        with open("../test/RequestShowData.json", "r") as tv_show_mock:
+            tv_show_dict = json.load(tv_show_mock)
+            for tv_show_name in tv_show_dict:
+                if tv_show_name == tv_show["show"]["name"]:
+                    return tv_show_dict[tv_show_name]
+            return tv_show_dict["Invincible"]
 
 
 class QuestionaryAdapter:
 
-    def __init__(self, questionary_library, tv_show_name):
+    def __init__(self, questionary_library, tv_show_name, tv_show_path="C:\Invincible.2021.S01"):
         self.questionary = questionary_library
         self.tv_show_name = tv_show_name
+        self.tv_show_path = tv_show_path
         self.answer = ""
 
     def ask(self):
@@ -77,14 +94,16 @@ class QuestionaryAdapter:
     def text(self, message):
         if message == "TV-show search:":
             self.answer = self.tv_show_name
+        elif message == "Input new path:":
+            self.answer = self.tv_show_path
 
         return self
 
     def confirm(self, message):
-        if message == "Is 'Invincible' correct TV-show?":
+        if message == f"Is '{self.tv_show_name}' correct TV-show?":
             self.answer = True
         elif message == "Use same path?":
-            self.answer = True
+            self.answer = False
         elif message == "Rename files?":
             self.answer = False
 
