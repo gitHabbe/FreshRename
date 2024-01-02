fb0eb7304140d03c8bb6306d6715a2420478e7f5
Habbe <20054400+gitHabbe@users.noreply.github.com>
2024-01-02 10:59:43 +0100

    ♻ Ref: more traditional main file

--- src/main.py
+++ src/main.py
@@ -3,16 +3,10 @@ from models.RequestShow import RequestShow
 from models.UserInput import UserInput
 
 
-class FreshRename:
-
-    @staticmethod
-    def run_command_line():
-        command_line = CommandLine(UserInput(), RequestShow())
-        command_line.run()
+def main():
+    command_line = CommandLine(UserInput(), RequestShow())
+    command_line.run()
 
 
 if __name__ == '__main__':
-    FreshRename().run_command_line()
-    # FreshRename().run_graphical()
-
-
+    main()
