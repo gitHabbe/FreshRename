8a9766347a6004f6c4a0a0f6125a34d643f2b308
Habbe <20054400+gitHabbe@users.noreply.github.com>
2024-01-14 12:00:36 +0100

    ♻ Ref: more specified imports

--- src/models/TvShowRootPath.py
+++ src/models/TvShowRootPath.py
@@ -1,21 +1,21 @@
 import json
-import os
+from os.path import dirname, abspath
 from dataclasses import dataclass
 from typing import ClassVar
 
 
 @dataclass
 class TvShowRootPath:
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    file_path: ClassVar[str] = f"{ROOT_DIR}/../private/cache.json"
+    __ROOT_DIR = dirname(abspath(__file__))
+    __path: ClassVar[str] = f"{__ROOT_DIR}/../private/cache.json"
 
     def get_path(self):
-        with open(self.file_path, "r") as file:
+        with open(self.__path, "r") as file:
             return json.load(file)
 
     def save_path(self, path: str):
         current_path = self.get_path()
         current_path["last_path"] = path
-        with open(self.file_path, "w") as file:
+        with open(self.__path, "w") as file:
             json.dump(current_path, file)
             return current_path["last_path"]
