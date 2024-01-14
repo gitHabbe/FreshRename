77470ab26aa68238ec831735b5521b959c5cb44e
Habbe <20054400+gitHabbe@users.noreply.github.com>
2024-01-14 11:03:48 +0100

    ♻ Ref: rearrange method orders

--- src/models/Rename.py
+++ src/models/Rename.py
@@ -13,6 +13,31 @@ class Rename:
     __name_strategy: NameStrategy
     fileList: list = field(default_factory=list)
 
+    def fill_file_list(self):
+        store = self.__dir_traverse.cache.store
+        for episode in self.__json_data:
+            season_num, episode_num = self.__uid(episode)
+            local_file = store.get(season_num + episode_num)
+            if local_file is None:
+                continue
+            self.__append_file_list(local_file, episode)
+            local_file = store.get(season_num + episode_num + "srt")
+            if local_file is None:
+                continue
+            self.__append_file_list(local_file, episode)
+
+    @staticmethod
+    def __uid(episode) -> list:
+        season_num = str(episode["season"]).zfill(2)
+        episode_num = str(episode["number"]).zfill(2)
+        return [season_num, episode_num]
+
+    def __append_file_list(self, local_file: LocalFileOriginal, episode) -> None:
+        file_item = self.__file_item(local_file, episode)
+        if file_item["oldName"] == file_item["newName"]:
+            return
+        self.fileList.append(file_item)
+
     def __file_item(self, local_file: LocalFileOriginal, episode) -> dict:
         self.__set_strategy(local_file)
         file_data = {}
@@ -30,33 +55,8 @@ class Rename:
         self.__name_strategy.season = season_num
         self.__name_strategy.episode = episode_num
 
-    def __append_file_list(self, local_file: LocalFileOriginal, episode) -> None:
-        file_item = self.__file_item(local_file, episode)
-        if file_item["oldName"] == file_item["newName"]:
-            return
-        self.fileList.append(file_item)
-
-    def fill_file_list(self):
-        store = self.__dir_traverse.cache.store
-        for episode in self.__json_data:
-            season_num, episode_num = self.__uid(episode)
-            local_file = store.get(season_num + episode_num)
-            if local_file is None:
-                continue
-            self.__append_file_list(local_file, episode)
-            local_file = store.get(season_num + episode_num + "srt")
-            if local_file is None:
-                continue
-            self.__append_file_list(local_file, episode)
-
     def rename_files(self):
         for singleFile in self.fileList:
             old_file = singleFile["oldFile"]
             new_file = singleFile["newFile"]
             os.rename(old_file, new_file)
-
-    @staticmethod
-    def __uid(episode) -> list:
-        season_num = str(episode["season"]).zfill(2)
-        episode_num = str(episode["number"]).zfill(2)
-        return [season_num, episode_num]
