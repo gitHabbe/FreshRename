2734cab033f12a7c58c6a47f42ce42e79aade3e3
Habbe <20054400+gitHabbe@users.noreply.github.com>
2024-01-26 14:55:34 +0100

    ✅ Test: adapt test to new changes. OSAction, OSMock and OSEntryMock

--- test/test_CommandLine.py
+++ test/test_CommandLine.py
@@ -1,14 +1,40 @@
 import json
-import os
 import unittest
 from unittest.mock import MagicMock
 
 from approvaltests.approvals import verify
 
+from OSAction import OSAction
 from frontend.CommandLine.CommandLine import CommandLine
-from main import OSAction
-from models.UserInput import UserInput
 from models.RequestShow import RequestShow
+from models.UserInput import UserInput
+
+
+class OSEntryMock:
+    def __init__(self, name: str, path: str, is_directory: bool):
+        self.name = name
+        self.path = path
+        self.is_directory = is_directory
+
+    def is_dir(self) -> bool:
+        return self.is_directory
+
+
+class OSMock:
+    def __init__(self, tv_show_name):
+        self.tv_show_name = tv_show_name
+
+    def scandir(self, path):
+        entries = []
+        with open("../test/LocalFileData.json", "r") as local_file_entries:
+            local_file_entries_data = json.load(local_file_entries)[self.tv_show_name]
+            for local_file_entry_data in local_file_entries_data:
+                entry = OSEntryMock(local_file_entry_data["name"], local_file_entry_data["path"], False)
+                entries.append(entry)
+        return entries
+
+    def rename(self, old_file_name, new_file_name):
+        pass
 
 
 class TestCommandLine(unittest.TestCase):
@@ -22,6 +48,7 @@ class TestCommandLine(unittest.TestCase):
         user_input.confirm_tv_show_path = MagicMock(side_effect=[False, True])
         user_input.choose_name_strategy = MagicMock(return_value="S01E01 - Episode name here")
         user_input.confirm_tv_show_rename = MagicMock(return_value=False)
+        user_input.confirm_tv_show_rename = MagicMock(return_value=True)
 
         request_show = RequestShow()
         request_show.name = MagicMock(return_value=[{"show": {"id": 559, "name": tv_show_name}}])
@@ -29,7 +56,9 @@ class TestCommandLine(unittest.TestCase):
             load = json.load(tv_show_mock)[tv_show_name]
             request_show.episodes = MagicMock(return_value=load)
 
-        self.__verify_tv_show(user_input, request_show)
+        action = OSAction(OSMock(tv_show_name))
+        os_action = action
+        self.__verify_tv_show(user_input, request_show, os_action)
 
     def test_tv_show_invincible(self):
         user_input = UserInput()
@@ -38,7 +67,7 @@ class TestCommandLine(unittest.TestCase):
         user_input.confirm_tv_show_name = MagicMock(return_value=True)
         user_input.ask_tv_show_path = MagicMock(return_value="C:\Invincible.2021.S01")
         user_input.confirm_tv_show_path = MagicMock(side_effect=[False, True])
-        user_input.choose_name_strategy = MagicMock(return_value="S01E01 - Episode name here")
+        user_input.choose_name_strategy = MagicMock(return_value="01x01 - Episode name here")
         user_input.confirm_tv_show_rename = MagicMock(return_value=False)
 
         request_show = RequestShow()
@@ -47,12 +76,14 @@ class TestCommandLine(unittest.TestCase):
             load = json.load(tv_show_mock)[tv_show_name]
             request_show.episodes = MagicMock(return_value=load)
 
-        self.__verify_tv_show(user_input, request_show)
+        action = OSAction(OSMock(tv_show_name))
+        os_action = action
+        self.__verify_tv_show(user_input, request_show, os_action)
 
-    def __verify_tv_show(self, user_input, request_show):
-        command_line = CommandLine(user_input, request_show, OSAction(os))
+    def __verify_tv_show(self, user_input, request_show, os_action):
+        command_line = CommandLine(user_input, request_show, os_action)
         command_line.run()
-        episode_list_data_as_string = self.__format_dict_list(command_line.test_data)
+        episode_list_data_as_string = self.__format_dict_list(os_action.rename.file_list)
         verify(episode_list_data_as_string)
 
     @staticmethod
