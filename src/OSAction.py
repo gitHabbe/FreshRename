31c9c38cbc1655a4f843c2cf6ac99fb194102f03
Habbe <20054400+gitHabbe@users.noreply.github.com>
2024-01-26 14:04:08 +0100

    ♻ Ref: OSAction create DirTraverse and Rename instead of injecting

--- src/OSAction.py
+++ src/OSAction.py
@@ -1,16 +1,17 @@
 from models.DirTraverse import DirTraverse
+from models.LocalFileEntry import LocalFileEntry
 from models.Rename import Rename
 
 
 class OSAction:
 
-    def __init__(self, operative_system, dir_traverse: DirTraverse, rename: Rename):
-        self.rename = rename
+    def __init__(self, operative_system):
         self.__os = operative_system
-        self.dir_traverse = dir_traverse
+        self.dir_traverse = DirTraverse(self.__os)
+        self.rename = Rename(self.__os)
 
-    def build_cache(self, new_path):
-        self.dir_traverse.build_cache(new_path)
+    def fill_file_list(self, store, episodes_data, name_strategy):
+        self.rename.fill_file_list(store, episodes_data, name_strategy)
 
-    def fill_file_list(self, episodes_data, name_strategy):
-        self.rename.fill_file_list(self.dir_traverse.cache.store, episodes_data, name_strategy)
+    def get_local_files(self, episodes_path):
+        return self.dir_traverse.get_local_files(episodes_path)
--- src/main.py
+++ src/main.py
@@ -2,19 +2,14 @@ import os
 
 from OSAction import OSAction
 from frontend.CommandLine.CommandLine import CommandLine
-from models.DirTraverse import DirTraverse
-from models.Rename import Rename
 from models.RequestShow import RequestShow
 from models.UserInput import UserInput
 
 
 def main():
-    dir_traverse = DirTraverse()
-    rename = Rename()
-    os_action = OSAction(os, dir_traverse, rename)
-
-    request_show = RequestShow()
     user_input = UserInput()
+    request_show = RequestShow()
+    os_action = OSAction(os)
 
     command_line = CommandLine(user_input, request_show, os_action)
     command_line.run()
